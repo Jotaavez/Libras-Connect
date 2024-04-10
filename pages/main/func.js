783906bd365c8875fb959acd515f9ad4723ffd5e
Jotave Reis <j.vitorfdosreis@gmail.com>
2024-04-10 20:29:23 -0300

    bastantes modificações

--- pages/main/func.js
+++ pages/main/func.js
@@ -1,9 +1,9 @@
 function abrirModal() {
-  const modal = document.getElementById("janela-modal");
+  const modal = document.getElementById("janela-modal-alfabeto");
   modal.classList.add("abrir");
 
   modal.addEventListener("click", (e) => {
-    if (e.target.id == "fechar" || e.target.id == "janela-modal") {
+    if (e.target.id == "fechar" || e.target.id == "janela-modal-alfabeto") {
       modal.classList.remove("abrir");
     }
   });
